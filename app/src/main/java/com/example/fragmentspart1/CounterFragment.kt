2bbb1916efe7d563c473d52c676336ec3593efae
Unixfusion <unixfusion@gmail.com>
2022-09-22 16:56:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/fragmentspart1/CounterFragment.kt
@@ -0,0 +1,48 @@
+package com.example.fragmentspart1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fragmentspart1.databinding.FragmentCounterBinding
+
+class CounterFragment : Fragment() {
+    private lateinit var binding: FragmentCounterBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCounterBinding.inflate(layoutInflater, container, false)
+
+        binding.tvScreenNumber.text = getString(R.string.screen_label, getCounterValue() )
+
+        binding.tvQuote.text = getQuote()
+        return binding.root
+    }
+
+    private fun goBack() {
+        requireActivity().onBackPressedDispatcher.onBackPressed()
+    }
+
+    private fun getCounterValue(): Int = requireArguments().getInt(ARG_COUNTER_VALUE)
+
+    private fun getQuote(): String = requireArguments().getString(ARG_QUOTE)!!
+
+    companion object {
+
+        @JvmStatic private val ARG_COUNTER_VALUE = "ARG_COUNTER_VALUE"
+        @JvmStatic private val ARG_QUOTE = "ARG_QUOTE"
+
+        @JvmStatic fun newInstance(counterValue: Int, quote: String): CounterFragment {
+            val args = Bundle().apply {
+                putInt(ARG_COUNTER_VALUE, counterValue)
+                putString(ARG_QUOTE, quote)
+            }
+            val fragment = CounterFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fragmentspart1/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.fragmentspart1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fragmentspart1.databinding.ActivityMainBinding
+import com.github.javafaker.Faker
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private  var faker = Faker.instance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        setSupportActionBar(binding.toolbar)
+
+        if (savedInstanceState == null) {
+            val fragment = CounterFragment.newInstance(
+                counterValue = 1,
+                quote = createQuote()
+            )
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit()
+        }
+    }
+
+    private fun createQuote(): String {
+        return faker.shakespeare().hamletQuote()
+    }
+}
